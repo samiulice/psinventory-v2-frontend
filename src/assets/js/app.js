474d6fcf555101308d26ec1b06976dbfcc6c801f
Samiul Bashir <coding.samiul@gmail.com>
2025-07-27 20:11:16 +0600

    Login url fixed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,7 @@ function terminateSession() {
   // Redirect to login page
   window.location.href = "login.html";
 }
-const api = 'https://psinventory-v2.onrender.com'
+const api = 'https://psinventory-v2.onrender.com/api/v2'
 // let api = 'http://localhost:8080/api/v2'
 function checkAuth() {
   console.log('Checking access level...');
