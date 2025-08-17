e669f081490f8d1f430909bf291040f70b59de8a
Samiul Bashir <coding.samiul@gmail.com>
2025-08-17 23:17:58 +0600

    fix: backend api url

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,24 +1,21 @@
 const { hostname, port } = window.location;
-
 let api;
-const cleanDomain = hostname.replace(/^www\./, ''); // remove www.
 
-// If the hostname contains "test" or "dev"
+const cleanDomain = hostname.replace(/^www\./, ''); // remove www.
+const mainDomain = cleanDomain.split('.').slice(-2).join('.'); // get main domain
+
 if (cleanDomain.includes('test') || cleanDomain.includes('dev')) {
-    // Use dev API
-    const mainDomain = cleanDomain.split('.').slice(-2).join('.'); // get main domain
-    api = `https://api.dev.${mainDomain}/api/v2`;
-}
-// For other pssoft.xyz domains
-else if (cleanDomain.endsWith('.pssoft.xyz')) {
-    // Use dev API
-    const mainDomain = cleanDomain.split('.').slice(-2).join('.'); // get main domain
-    
-    api = `https://api.${mainDomain}/api/v2`;
-}
-// Fallback
-else {
-    api = `http://localhost:${port || 8000}/api/v2`;
+  // Test or dev subdomains
+  api = `https://api.dev.${mainDomain}/api/v2`;
+} else if (cleanDomain.includes('demo')) {
+  // Demo subdomain
+  api = `https://api.demo.${mainDomain}/api/v2`;
+} else if (cleanDomain.endsWith('.pssoft.xyz')) {
+  // Other pssoft.xyz domains
+  api = `https://api.${mainDomain}/api/v2`;
+} else {
+  // Fallback to localhost
+  api = `http://localhost:${port || 8000}/api/v2`;
 }
 
 console.log("API Base:", api);
